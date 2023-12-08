d807b16315f24fc83697f419c7421806661d62a4
Hafiz Muhammad Tahir <150361927+tahircoder2344@users.noreply.github.com>
2023-12-08 16:56:52 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const endDate = "9 December 2023, 4:47 AM";
+const inputs = document.querySelectorAll("input");
+document.getElementById("end-date").innerText = endDate;
+
+const clock = () => {
+  const end = new Date(endDate);
+  const now = new Date();
+  const diff = (end - now) / 1000;
+  console.log(diff);
+  //for days
+  inputs[0].value = Math.floor(diff / 3600 / 24);
+  // for hours
+  inputs[1].value = Math.floor(diff / 3600) % 24;
+  inputs[2].value = Math.floor(diff / 60) % 60;
+  inputs[3].value = Math.floor(diff) % 60;
+
+  // conersion into days;
+};
+
+clock();
+
+
+
+setInterval(
+    () => {
+        clock();
+    }
+)
